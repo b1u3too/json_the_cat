8dcdf38cc202aadae0f0696dbf3c8a176edf8831
b1u3too <almadsen792@live.ca>
2022-02-26 21:08:35 +0000

    modularize function for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,21 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request(`https://api.thecata/pi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error || response.responseCode !== 200) {
-    console.log(error);
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, _response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
     const data = JSON.parse(body);
     const requestedCat = data[0];
-    console.log(requestedCat === undefined ? 'Error: Requested breed not found!' : requestedCat.description);
-  }
-});
+
+    if (requestedCat) {
+      callback(null, requestedCat.description);
+    } else {
+      callback("Error: Requested breed not found!");
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
