706370291d1b1193ed31cc8950d47d315ebc0b70
b1u3too <almadsen792@live.ca>
2022-02-26 20:15:25 +0000

    add error handling for unfound request and request fail

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,12 @@
 const request = require('request');
 const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  const requestedCat = data[0];
-  console.log(requestedCat.description);
+request(`https://api.thecata/pi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  if (error || response.responseCode !== 200) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const requestedCat = data[0];
+    console.log(requestedCat === undefined ? 'Error: Requested breed not found!' : requestedCat.description);
+  }
 });
